ce63051a29314c15712f654667175af7aeb9f088
FlourCode <mark.flournoy@gmail.com>
2025-05-01 12:58:32 -0400

    Add files via upload

--- test_script.js
+++ test_script.js
@@ -445,7 +445,7 @@ function displayContractLeadersTable(leaderData) {
     const tableWrapper = document.createElement('div');
     tableWrapper.className = 'table-wrapper';
     tableWrapper.style.overflow = 'auto';
-    tableWrapper.style.maxHeight = '300px';
+    tableWrapper.style.maxHeight = 'none';
 
     // Display top 15 leaders
     const displayData = leaderData.slice(0, 15);
