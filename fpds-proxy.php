2fbc7a1515ffe494c020b6e0aa6ca0fde861a4b3
FlourCode <mark.flournoy@gmail.com>
2025-05-23 11:16:17 -0400

    Add files via upload

--- fpds-proxy.php
+++ fpds-proxy.php
@@ -2,13 +2,20 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = "https://www.fpds.gov/ezsearch/FEEDS/ATOM?q=PRINCIPAL_NAICS_CODE:%22511210%22%20CREATED_DATE:[2023/05/01,2025/05/23]";
-$xml = simplexml_load_file($url);
-$ns = $xml->getNamespaces(true);
+$url = "https://www.fpds.gov/ezsearch/fpdsportal?s=FPDS&indexName=awardfull&templateName=1.5.3&q=PRINCIPAL_NAICS_CODE:%22511210%22++CREATED_DATE:[2023/05/01,2025/05/23]&rss=1&feed=atom0.3";
+
+$xml = @simplexml_load_file($url);
+
+if (!$xml) {
+    echo json_encode(["error" => "Failed to load XML from FPDS"]);
+    exit;
+}
+
 $results = [];
 
 foreach ($xml->entry as $entry) {
-    $content = $entry->children($ns['']);
+    $ns = $entry->getNamespaces(true);
+    $content = $entry->children($ns[''])->content;
     $award = $content->children($ns['ns1'])->award;
 
     $agency = (string)$award->purchaserInformation->contractingOfficeAgencyID['name'];
